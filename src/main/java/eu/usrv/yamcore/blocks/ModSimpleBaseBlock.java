245c451696b9428c35a298beb4d7358889f0871d
Namikon <namikon@gmx.net>
2015-05-19 10:30:38 +0200

    Finished BlockHandling

--- src/main/java/eu/usrv/yamcore/blocks/ModSimpleBaseBlock.java
+++ src/main/java/eu/usrv/yamcore/blocks/ModSimpleBaseBlock.java
@@ -2,6 +2,7 @@ package eu.usrv.yamcore.blocks;
 
 import eu.usrv.yamcore.YAMCore;
 import eu.usrv.yamcore.auxiliary.LogHelper;
+import eu.usrv.yamcore.iface.IExtendedBlockProperties;
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
 import net.minecraft.creativetab.CreativeTabs;
@@ -30,6 +31,57 @@ public class ModSimpleBaseBlock {
 		_mBlockInstance = new BlockBase(pBlockBaseMaterial);
 		_mBlockInstance.setBlockName(pBlockName);
 	}
+	
+	/**
+	 * Create a new (still simple) Block by using a helper class to provide additional Information about the block's properties
+	 * @param pBlockProperties
+	 */
+	public ModSimpleBaseBlock(IExtendedBlockProperties pBlockProperties, String pCreativeTabName)
+	{
+		_mName = pBlockProperties.getBlockName();
+		_mCreativeTab = pCreativeTabName;
+		_mTextureOverride = pBlockProperties.getTextureName();
+		
+		_mBlockInstance = new BlockBase(pBlockProperties.getMaterial());
+		_mBlockInstance.setBlockName(_mName);
+		
+		if (pBlockProperties.getUnbreakable())
+			_mBlockInstance.setBlockUnbreakable();
+		else
+		{
+			_mBlockInstance.setHardness(pBlockProperties.getHardness());
+			_mBlockInstance.setHarvestLevel(pBlockProperties.getHarvestToolClass(), pBlockProperties.getHarvestLevel());
+			_mBlockInstance.setResistance(pBlockProperties.getResistance());
+		}
+		_mBlockInstance.setStepSound(pBlockProperties.getStepSound());
+		_mBlockInstance.setLightLevel(pBlockProperties.getLightLevel());
+		_mBlockInstance.setLightOpacity(pBlockProperties.getOpacity());
+	}
+	
+	/**
+	 * Create a new simple block (for recipes or other very basic stuff)
+	 * @param pItemName The Blocks unlocalized name
+	 * @param pCreativeTabName The name for the creative tab that will be assigned later in the registration process
+	 */
+	public ModSimpleBaseBlock(Material pBlockBaseMaterial, String pBlockName, String pCreativeTabName)
+	{
+		this(pBlockBaseMaterial, pBlockName, "", pCreativeTabName);
+	}
+	
+	public void setModIDName(String pModID)
+	{
+		
+		String tTextureName = "";
+		if (_mTextureOverride.length() < 1)
+			tTextureName = String.format("%s:block%s", pModID, _mName);
+		else
+			tTextureName = _mTextureOverride;
+			//tTextureName = String.format("%s:%s", pModID, _mTextureOverride);
+		
+		_mBlockInstance.setBlockTextureName(tTextureName);
+		_mFullyPopulated = true;
+	}
+
 	
 	public BlockBase getConstructedBlock()
 	{
---
+++ src/main/java/eu/usrv/yamcore/iface/IExtendedBlockProperties.java
@@ -0,0 +1,74 @@
+package eu.usrv.yamcore.iface;
+
+import net.minecraft.block.Block.SoundType;
+import net.minecraft.block.material.Material;
+
+public interface IExtendedBlockProperties {
+	public Material getMaterial();
+	
+	
+	/**
+	 * Unbreakable sand anyone?
+	 * @return
+	 */
+	public boolean getUnbreakable();
+	
+	
+	/**
+	 * The block's harvest level. Vanilla values are: 0: wood 3: diamond
+	 * @return
+	 */
+	public int getHarvestLevel();
+	
+	
+	/** 
+	 * The higher this number is, the more light is beeing blocked by this block
+	 * @return
+	 */
+	public int getOpacity();
+
+	/**
+	 * Can be ignored if getHarvestLevel() will return true
+	 * @return
+	 */
+	public float getHardness();
+	
+	
+	/** How much light will this block emit. 0.0F = nothing, 1.0F = daylight
+	 * @return
+	 */
+	public float getLightLevel();
+
+	/**
+	 * Can be ignored if getHarvestLevel() will return true
+	 * @return
+	 */
+	public float getResistance();
+
+	
+	/**
+	 * The Blockname that is used to register this block
+	 * @return
+	 */
+	public String getBlockName();
+	
+	
+	/**
+	 * The custom textureName. Return an empty string to use default texture mapping
+	 * @return
+	 */
+	public String getTextureName();
+
+	/**
+	 * Can be ignored if getHarvestLevel() will return true
+	 * @return
+	 */
+	public String getHarvestToolClass();
+	
+	
+	/**
+	 * The sound this block will make if any entity walks over it
+	 * @return
+	 */
+	public SoundType getStepSound();
+}	
