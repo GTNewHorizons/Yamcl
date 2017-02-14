8f9cb0513a08c931de726c30dec6140fb506d33c
Namikon <namikon@gmx.net>
2017-02-14 17:17:23 +0100

    MySQL Database toolset

---
+++ src/main/java/eu/usrv/yamcore/datasource/bridge/Bridge.java
@@ -0,0 +1,14 @@
+
+package eu.usrv.yamcore.datasource.bridge;
+
+
+/**
+ * Used for handling connection to a database through a database engine specified.
+ */
+public abstract class Bridge
+{
+  public Bridge()
+  {
+
+  }
+}
