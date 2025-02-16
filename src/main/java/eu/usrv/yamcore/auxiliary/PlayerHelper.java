84fbca1fc02efa5cd6853c424147ce44f5fe86a9
Discreater <discreater@163.com>
2025-02-16 11:25:22 +0800

    Add missing localization

--- src/main/java/eu/usrv/yamcore/auxiliary/PlayerHelper.java
+++ src/main/java/eu/usrv/yamcore/auxiliary/PlayerHelper.java
@@ -2,6 +2,7 @@
 package eu.usrv.yamcore.auxiliary;
 
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.util.StatCollector;
 import net.minecraft.util.Vec3;
 import net.minecraftforge.common.util.FakePlayer;
 
@@ -9,15 +10,15 @@ import eu.usrv.yamcore.YAMCore;
 
 /**
  * Various functions for/with Players (EntityPlayer)
- * 
+ *
  * @author Namikon
- * 
+ *
  */
 public class PlayerHelper {
 
     /**
      * Remove protection from the player
-     * 
+     *
      * @param pEntityPlayer
      */
     public static void RemoveProtection(EntityPlayer pEntityPlayer) {
@@ -26,7 +27,7 @@ public class PlayerHelper {
 
     /**
      * Actually GIVE protection to the player and notify the player
-     * 
+     *
      * @param pEntityPlayer
      */
     public static void GiveProtection(EntityPlayer pEntityPlayer) {
@@ -35,7 +36,7 @@ public class PlayerHelper {
 
     /**
      * Remove protection from the player
-     * 
+     *
      * @param pEntityPlayer
      */
     public static void RemoveProtection(EntityPlayer pEntityPlayer, boolean pNotifyPlayer) {
@@ -43,7 +44,7 @@ public class PlayerHelper {
             pEntityPlayer.capabilities.disableDamage = false;
             pEntityPlayer.sendPlayerAbilities();
             if (pNotifyPlayer)
-                PlayerChatHelper.SendNotifyWarning(pEntityPlayer, "Your magic bubble of protection fades...");
+                PlayerChatHelper.SendNotifyWarning(pEntityPlayer, StatCollector.translateToLocal("yamcore.char.protection.remove"));
         } catch (Exception e) {
             YAMCore.instance.getLogger().error("RemoveProtection failed to Remove player's protection");
             YAMCore.instance.getLogger().DumpStack(e);
@@ -52,7 +53,7 @@ public class PlayerHelper {
 
     /**
      * Actually GIVE protection to the player
-     * 
+     *
      * @param pEntityPlayer
      * @param pNotifyPlayer notify the player about the change
      */
@@ -61,7 +62,7 @@ public class PlayerHelper {
             pEntityPlayer.capabilities.disableDamage = true;
             pEntityPlayer.sendPlayerAbilities();
             if (pNotifyPlayer)
-                PlayerChatHelper.SendNotifyPositive(pEntityPlayer, "A magic bubble of protection appears...");
+                PlayerChatHelper.SendNotifyPositive(pEntityPlayer, StatCollector.translateToLocal("yamcore.char.protection.give"));
         } catch (Exception e) {
             YAMCore.instance.getLogger().error("RemoveProtection failed to give a player protection");
             YAMCore.instance.getLogger().DumpStack(e);
@@ -72,11 +73,11 @@ public class PlayerHelper {
      * Add a distance of (approximately) pDistance blocks to a players current location. Height (y) is not affected by
      * this, only X/Z Coordinates are. The current direction (North/South/East/West, also known as "Yaw") is used to
      * calculate the new location
-     * 
+     *
      * Note: This is usually used Server-Side, which means the Result might be "wrong". This is because the server isn't
      * always up to date about a players exact Yaw. If you want a more accurate result, use addDistanceByVecAndYaw() and
      * grab the current Yaw by other means (Custom Packets,..)
-     * 
+     *
      * @param pPlayer   The player; Source location
      * @param pDistance The distance (in blocks) to be added
      * @return A Vector describing the new location
@@ -93,7 +94,7 @@ public class PlayerHelper {
      * Add a distance of (approximately) pDistance blocks to a given Vector. Height (y) is not affected by this, only
      * X/Z Coordinates are. The current direction (North/South/East/West, also known as "Yaw") is used to calculate the
      * new location
-     * 
+     *
      * @param pSourceLocation The source location as Vector
      * @param pYaw            The Yaw (Usually taken from rotationYaw at EntityPlayer)
      * @param pDistance       The distance (in blocks) to be added
